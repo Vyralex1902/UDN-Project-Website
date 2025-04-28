f8465bb5f19c933e3f637521848cc5c94f63cac1
Cyberspark <lucammontanari@gmail.com>
2025-04-28 13:59:31 +0200

    up

--- app/heading.tsx
+++ app/heading.tsx
@@ -6,6 +6,8 @@ import { FaHistory } from "react-icons/fa";
 import { IoBag } from "react-icons/io5";
 import { motion } from "framer-motion";
 
+import { RxDividerHorizontal } from "react-icons/rx";
+
 
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
@@ -25,6 +27,7 @@ const Heading = () => {
     const headingIconElement2 = document.getElementById("headingIcon2");
     const headingIconElement3 = document.getElementById("headingIcon3");
     const headingIconElement4 = document.getElementById("headingIcon4");
+    const headingIconElement5 = document.getElementById("headingIcon5");
 
     headingIconElement1!.onclick = (e) => {
       headingIconElement1!.classList.remove("heading-icon");
@@ -34,28 +37,8 @@ const Heading = () => {
       headingIconElement3!.classList.add("heading-icon");
       headingIconElement4!.classList.remove("heading-icon-selected");
       headingIconElement4!.classList.add("heading-icon");
-      headingIconElement2!.classList.remove("heading-icon-selected");
-      headingIconElement2!.classList.add("heading-icon");
-    }
-    headingIconElement3!.onclick = (e) => {
-      headingIconElement3!.classList.remove("heading-icon");
-      headingIconElement3!.classList.add("heading-icon-selected");
-
-      headingIconElement1!.classList.remove("heading-icon-selected");
-      headingIconElement1!.classList.add("heading-icon");
-      headingIconElement4!.classList.remove("heading-icon-selected");
-      headingIconElement4!.classList.add("heading-icon");
-      headingIconElement2!.classList.remove("heading-icon-selected");
-      headingIconElement2!.classList.add("heading-icon");
-    }
-    headingIconElement4!.onclick = (e) => {
-      headingIconElement4!.classList.remove("heading-icon");
-      headingIconElement4!.classList.add("heading-icon-selected");
-
-      headingIconElement1!.classList.remove("heading-icon-selected");
-      headingIconElement1!.classList.add("heading-icon");
-      headingIconElement3!.classList.remove("heading-icon-selected");
-      headingIconElement3!.classList.add("heading-icon");
+      headingIconElement5!.classList.remove("heading-icon-selected");
+      headingIconElement5!.classList.add("heading-icon");
       headingIconElement2!.classList.remove("heading-icon-selected");
       headingIconElement2!.classList.add("heading-icon");
     }
@@ -68,57 +51,26 @@ const Heading = () => {
 
       headingIconElement3!.classList.remove("heading-icon-selected");
       headingIconElement3!.classList.add("heading-icon");
+      headingIconElement2!.classList.remove("heading-icon-selected");
+      headingIconElement2!.classList.add("heading-icon");
       headingIconElement4!.classList.remove("heading-icon-selected");
       headingIconElement4!.classList.add("heading-icon");
-    }
+      headingIconElement5!.classList.remove("heading-icon-selected");
+      headingIconElement5!.classList.add("heading-icon");
+    } //TODO: complete with other pages
 
-    if (
-      document.getElementById("pageName")?.getAttribute('value') == "buyservice"
-      || document.getElementById("pageName")?.getAttribute('value') == "home"
-      || document.getElementById("pageName")?.getAttribute('value') == "mystory"
-      || document.getElementById("pageName")?.getAttribute('value') == "hireme"
-    ) {
-      if (localStorage.getItem('color-theme')) {
-        if (localStorage.getItem('color-theme') === 'light') {
-          document.documentElement.classList.add('dark');
-          localStorage.setItem('color-theme', 'dark');
-        }
-
-        // if NOT set via local storage previously
-      } else {
-        if (document.documentElement.classList.contains('light')) {
-          document.documentElement.classList.add('dark');
-          localStorage.setItem('color-theme', 'dark');
-        }
-      }
-    }
-    else if (document.getElementById("pageName")?.getAttribute('value') == "meridian") {
-      if (localStorage.getItem('color-theme')) {
-        if (localStorage.getItem('color-theme') === 'dark') {
-          document.documentElement.classList.remove('dark');
-          localStorage.setItem('color-theme', 'light');
-        }
-
-        // if NOT set via local storage previously
-      } else {
-        if (document.documentElement.classList.contains('dark')) {
-          document.documentElement.classList.remove('dark');
-          localStorage.setItem('color-theme', 'light');
-        }
-      }
-    }
-
-    console.log("Heading loaded.");
+    console.log("Navbar loaded.");
   });
 
   return (
     <div className="fixed top-0 flex flex-row pt-[6px] pb-1 z-50 justify-center w-screen h-[75px] cursor-pointer">
-      <div id="headingThing" className="min-w-[550px] max-w-[550px] backdrop-blur-lg border-gray-500 dark:border-gray-700 border-opacity-20 dark:border-opacity-100 border-[1.5px] flex w-fit justify-center rounded-full items-center bg-transparent">
+      <div id="headingThing" className="min-w-[670px] max-w-[550px] backdrop-blur-lg border-gray-500 dark:border-gray-700 border-opacity-20 dark:border-opacity-100 border-[1.5px] flex w-fit justify-center rounded-full items-center bg-transparent">
         <div className="flex w-fit justify-center absolute justify-self-center gap-2" style={{ width: "100%" }}>
           <HeadingIcon id="headingIcon1" text="Home" link="/" ></HeadingIcon>
-          <DevelopmentHeadingIcon />
-          <HeadingIcon id="headingIcon3" text="Passions" link="/hobbies" ></HeadingIcon>
-          <HeadingIcon id="headingIcon4" text="My Story" link="/mystory" ></HeadingIcon>
+          <ProductsHeadingIcon />
+          <CommunityHeadingIcon />
+          <LearnHeadingIcon />
+          <NewsHeadingIcon />
         </div>
       </div>
     </div>
@@ -133,67 +85,76 @@ const HeadingIcon = ({ id, text = 'Text', link }: { id: string, text: string, li
   </Link>
 );
 
-const DevelopmentHeadingIcon = () => (
+const ProductsHeadingIcon = () => (
   <div>
     <div id="headingIcon2" className="heading-icon group z-50 text-[15px] cursor-pointer">
-      Development
+      Products
       <div className="absolute" id="dropdown-content">
         <Link to="programming" className="cursor-pointer">
-          <p id="di1" className="text-black dark:text-white bg-white dark:bg-gray-900 hover:bg-gray-300 hover:dark:bg-gray-800">My Knowledge</p>
+          <p id="di1" className="text-white bg-gray-900 hover:bg-gray-800">Apps</p>
         </Link>
         <Link to="docs" className="cursor-pointer">
-          <p id="di2" className="text-black dark:text-white bg-white dark:bg-gray-900 hover:bg-gray-300 hover:dark:bg-gray-800">My Docs</p>
+          <p id="di2" className="text-white bg-gray-900 hover:bg-gray-800">Docs</p>
         </Link>
         <Link to="myprojects" className="cursor-pointer">
-          <p id="di3" className="text-black dark:text-white bg-white dark:bg-gray-900 hover:bg-gray-300 hover:dark:bg-gray-800">My Projects</p>
+          <p id="di3" className="text-white bg-gray-900 hover:bg-gray-800">Design</p>
+        </Link>
+      </div>
+    </div>
+  </div>
+);
+const CommunityHeadingIcon = () => (
+  <div>
+    <div id="headingIcon3" className="heading-icon group z-50 text-[15px] cursor-pointer">
+      Community
+      <div className="absolute" id="dropdown-content">
+        <Link to="programming" className="cursor-pointer">
+          <p id="di4" className="text-black dark:text-white bg-white dark:bg-gray-900 hover:bg-gray-300 hover:dark:bg-gray-800">Threads</p>
+        </Link>
+        <Link to="docs" className="cursor-pointer">
+          <p id="di5" className="text-black dark:text-white bg-white dark:bg-gray-900 hover:bg-gray-300 hover:dark:bg-gray-800">Sign up</p>
+        </Link>
+      </div>
+    </div>
+  </div>
+);
+const LearnHeadingIcon = () => (
+  <div>
+    <div id="headingIcon4" className="heading-icon group z-50 text-[15px] cursor-pointer">
+      Learn
+      <div className="absolute" id="dropdown-content">
+        <Link to="programming" className="cursor-pointer">
+          <p id="di6" className="text-black dark:text-white bg-white dark:bg-gray-900 hover:bg-gray-300 hover:dark:bg-gray-800">Courses</p>
+        </Link>
+        <Link to="docs" className="cursor-pointer">
+          <p id="di7" className="text-black dark:text-white bg-white dark:bg-gray-900 hover:bg-gray-300 hover:dark:bg-gray-800">Libraries</p>
+        </Link>
+        <Link to="myprojects" className="cursor-pointer">
+          <p id="di8" className="text-black dark:text-white bg-white dark:bg-gray-900 hover:bg-gray-300 hover:dark:bg-gray-800">Tech Stacks</p>
         </Link>
         <Link to="meridian" className="cursor-pointer">
-          <p id="di4" className="text-black dark:text-white bg-white dark:bg-gray-900 hover:bg-gray-300 hover:dark:bg-gray-800">Project Meridian</p>
+          <p id="di9" className="text-black dark:text-white bg-white dark:bg-gray-900 hover:bg-gray-300 hover:dark:bg-gray-800">Snippets</p>
+        </Link>
+      </div>
+    </div>
+  </div>
+); const NewsHeadingIcon = () => (
+  <div>
+    <div id="headingIcon5" className="heading-icon group z-50 text-[15px] cursor-pointer">
+      News
+      <div className="absolute" id="dropdown-content">
+        <Link to="programming" className="cursor-pointer">
+          <p id="di10" className="text-black dark:text-white bg-white dark:bg-gray-900 hover:bg-gray-300 hover:dark:bg-gray-800">Newsletter</p>
+        </Link>
+        <Link to="docs" className="cursor-pointer">
+          <p id="di11" className="text-black dark:text-white bg-white dark:bg-gray-900 hover:bg-gray-300 hover:dark:bg-gray-800">Blog</p>
+        </Link>
+        <Link to="myprojects" className="cursor-pointer">
+          <p id="di12" className="text-black dark:text-white bg-white dark:bg-gray-900 hover:bg-gray-300 hover:dark:bg-gray-800">Social media</p>
         </Link>
       </div>
     </div>
   </div>
 );
 
-const toggleTheme = () => {
-  if (
-    document.getElementById("pageName")?.getAttribute('value') == "buyservice"
-    || document.getElementById("pageName")?.getAttribute('value') == "home"
-    || document.getElementById("pageName")?.getAttribute('value') == "mystory"
-    || document.getElementById("pageName")?.getAttribute('value') == "hireme"
-    || document.getElementById("pageName")?.getAttribute('value') == "meridian"
-  ) {
-    switchableTheme = false;
-  }
-  else {
-    switchableTheme = true;
-  }
-
-  if (switchableTheme) {
-    if (localStorage.getItem('color-theme')) {
-      if (localStorage.getItem('color-theme') === 'light') {
-        document.documentElement.classList.add('dark');
-        localStorage.setItem('color-theme', 'dark');
-      } else {
-        document.documentElement.classList.remove('dark');
-        localStorage.setItem('color-theme', 'light');
-      }
-
-      // if NOT set via local storage previously
-    } else {
-      if (document.documentElement.classList.contains('dark')) {
-        document.documentElement.classList.remove('dark');
-        localStorage.setItem('color-theme', 'light');
-      } else {
-        document.documentElement.classList.add('dark');
-        localStorage.setItem('color-theme', 'dark');
-      }
-    }
-    console.log("Theme toggled.");
-  }
-  else {
-
-    Swal.fire("Can't do that", "Sorry, you can't switch to dark theme on this page. I locked it either because it only looks good in light mode or because these is no need to switch it.", "warning");
-  }
-}
 export default Heading;
